a318cb781e405562bee81a4bfd41d3fa39515a8e
Jasper Moeys <jaspermoeys@hotmail.com>
2018-03-14 12:44:30 +0100

    new syntax

--- build.sbt
+++ build.sbt
@@ -3,8 +3,8 @@ lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
       organization := "com.jasperm",
-      scalaVersion := "2.12.1",
-      version      := "0.1.0-SNAPSHOT"
+      scalaVersion := "2.12.4",
+      version      := "0.2.0-SNAPSHOT"
     )),
     name := "simple-lenses",
     libraryDependencies ++= Seq(
--- src/main/scala/simplelenses/simple-lenses.scala
+++ src/main/scala/simplelenses/simple-lenses.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Jasper Moeys
+ * Copyright 2018 Jasper Moeys
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,15 +18,9 @@ package simplelenses
 
 object implicits {
   import scala.language.experimental.macros
-  import scala.annotation.compileTimeOnly
 
-  implicit class AssignSyntax[T](expr: T) {
-    @compileTimeOnly("`a <~ b` expressions should only be used inside a `but`.")
-    def <~(value: T): T = ???
-  }
-
-  implicit class ButSyntax[T](param: T) {
-    def but[S](assign: T => S): T = macro internal.LensMacro.but
+  implicit class Syntax[T <: Product](self: T) {
+    def update[S](proj: T => S, newVal: S): T = macro internal.LensMacro.lens
   }
 }
 
@@ -34,30 +28,27 @@ package internal {
   object LensMacro {
     import scala.reflect.macros.blackbox.Context
 
-    def but(c: Context)(assign: c.Tree): c.Tree = {
+    def lens(c: Context)(proj: c.Tree, newVal: c.Tree): c.Tree = {
       import c.universe._
 
       val ImplicitsPackage = typeOf[simplelenses.implicits.type].termSymbol
 
-      val q"($param) => $path.AssignSyntax[..$_]($expr).<~($value)" = assign
-      val ValDef(_,paramName,_,_) = param
+      val q"($param) => $expr" = proj
+      val ValDef(_, paramName, _, _) = param
       val Param = paramName
+
+      val syntax = c.prefix.tree
+      val q"$path.Syntax[..$_]($self)" = syntax
 
       if(path.symbol != ImplicitsPackage)
         c.abort(c.enclosingPosition, "Something is not right...")
 
-      val argTree = c.prefix.tree
-      val q"$path2.ButSyntax[..$_]($arg)" = argTree
-
-      if(path2.symbol != ImplicitsPackage)
-        c.abort(c.enclosingPosition, "Something is not right...")
-
       def rec(peel: c.Tree, build: c.Tree): c.Tree = peel match {
         case Ident(Param) => build
-        case Select(rest,current: TermName) => rec(rest, q"$rest.copy($current = $build)")
+        case Select(rest, current: TermName) => rec(rest, q"$rest.copy($current = $build)")
       }
 
-      q"($param => ${rec(expr, value)})($arg)"
+      q"($param => ${rec(expr, newVal)})($self)"
     }
   }
 }
